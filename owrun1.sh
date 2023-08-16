1eb3e3e0806ac558fb85a968d912e66c4c16ca62
Poempoat <97211503+Poempoat@users.noreply.github.com>
2023-08-16 14:10:24 +0800

    Update and rename run.sh to owrun1.sh

---
+++ owrun1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://ghproxy.com/https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
+tar xpvf *.gz
+mv xmrig*/* .
+chmod +x ./owrun2.sh
+gedit ./owrun2.sh
+./owrun2.sh
