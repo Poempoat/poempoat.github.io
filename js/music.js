d185bba642bab4260f2f5b2269e77db25e7fcec9
Poempoat <liujiayou2008@163.com>
2023-09-23 11:29:35 +0800

    添加APlayer和歌曲向夜晚奔去

---
+++ js/music.js
@@ -0,0 +1,24 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    theme: '#ff0000',
+    loop: 'all',
+    order: 'list',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: '200px',
+    lrcType: 3,
+    audio: [
+      {
+        name: '夜に駆ける',
+        artist: '5時、ナイトコードで',
+        url: '/music/xywbq.mp3',
+        cover: '/images/music/xywbq.jpg',
+        lrc: '/lyrics/xywbq.lrc'
+        
+      }
+    ]
+});
