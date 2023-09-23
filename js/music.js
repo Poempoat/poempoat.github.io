0fdcfa72c6d14ed665cad0108337f3b5f19f052a
Poempoat <liujiayou2008@163.com>
2023-09-23 14:35:46 +0800

    添加友链列表

--- js/music.js
+++ js/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: true,
+    autoplay: false,
     theme: '#ff0000',
     loop: 'all',
     order: 'list',
